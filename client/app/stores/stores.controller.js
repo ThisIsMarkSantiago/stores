94aaaf499270ac982d56b9e033327c3e4c46a48d
ThisIsMarkSantiago <mesantiago@dynamicobjx.com>
2016-11-21 16:24:26 +0800

    Store list page
    with delete function

--- client/app/stores/stores.controller.js
+++ client/app/stores/stores.controller.js
@@ -3,20 +3,33 @@
 (function(){
 
 class StoresComponent {
-  constructor($http) {
-    angular.merge( this, {
-      $http,
-      loading: {}
+  constructor(API, Modal) {
+    angular.extend( this, {
+      API,
+      Modal,
+      loading: {},
+      errors: {}
     });
   }
 
   $onInit() {
-    this.loading.stores = true;
-    this.$http.get('/api/stores')
-      .then(response => {
-        this.stores = response.data;
-      })
-      .finally(() => this.loading.stores = false);
+    this.loading.list = true;
+    this.errors.list = undefined;
+    this.API.get('/stores')
+      .then(stores => this.stores = stores)
+      .catch(() => this.errors.list = 'Something went wrong in fetching list of stores. Try to reload the page.')
+      .finally(() => this.loading.list = false);
+  }
+
+  delete(store) {
+    this.Modal.confirm.delete(() => {
+      this.loading.delete = true;
+      this.errors.delete = undefined;
+      this.API.delete(`/stores/${store._id}`)
+        .then(() => this.stores.splice(this.stores.indexOf(store), 1))
+        .catch(() => this.errors.delete = 'Something went wrong in deleting the store. Try again.')
+        .finally(() => this.loading.delete = false);
+    })(store.name + ' store');
   }
 }
 
