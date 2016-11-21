84a83442ca31b9f26081f7825a72e4f12901b612
ThisIsMarkSantiago <mesantiago@dynamicobjx.com>
2016-11-21 16:21:15 +0800

    Use API service in Admin page
    add loading

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -3,14 +3,35 @@
 (function() {
 
   class AdminController {
+
     constructor(User) {
-      // Use the User $resource to fetch all users
-      this.users = User.query();
+      angular.extend(this, {
+        User,
+        loading: {},
+        errors: {}
+      });
+
+      this.getUsersList();
+    }
+
+    getUsersList() {
+      this.loading.list = true;
+      this.errors.list = undefined;
+      this.users = this.User.query();
+
+      // Fallback when fetchingfailed
+      this.users.$promise
+        .catch(() => this.errors.list = 'Something went wrong in fetching users list.')
+        .finally(() => this.loading.list = false);
     }
 
     delete(user) {
-      user.$remove();
-      this.users.splice(this.users.indexOf(user), 1);
+      this.loading.delete = true;
+      this.errors.delete = undefined;
+      this.User.delete({ id: user._id }).$promise
+        .then(() => this.users.splice(this.users.indexOf(user), 1))
+        .catch(() => this.errors.delete = 'Unable to delete user.')
+        .finally(() => this.loading.delete = false);
     }
   }
 
